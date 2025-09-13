b4d1729743f2322042395536175531c20b4f485a
unknown <anhelina.pryadko@gmail.com>
2025-09-13 10:31:07 +0300

    add validation schema, write /add method, which should be tested and improved if it is necessary

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import { validationResult, checkSchema } from "express-validator";
 import { dbConfig } from "./config/dataBaseConfig.js";
+import {
+  titleAddValidationSchema,
+  titleUpdateValidationSchema,
+} from "./utilities/validationSchema.mjs";
 
 const app = express();
 const port = 3000;
@@ -12,15 +17,23 @@ app.use(express.static("public"));
 let db = new pg.Client(dbConfig);
 db.connect();
 
-async function getItems(){
+async function getItems() {
   let items = [];
-  try{
+  try {
     const result = await db.query("select * from items");
     items = result.rows;
-  } catch (error){
+  } catch (error) {
     console.log(error);
   }
   return items;
+}
+
+async function addItem(title) {
+  try {
+    await db.query("insert into items (title) values ($1)", [title]);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 app.get("/", async (req, res) => {
@@ -31,9 +44,13 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.post("/add", (req, res) => {
-  const item = req.body.newItem;
-  items.push({ title: item });
+app.post("/add", checkSchema(titleAddValidationSchema), async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+  } else {
+    await addItem(req.body.newItem);
+  }
   res.redirect("/");
 });
 
---
+++ utilities/validationSchema.mjs
@@ -0,0 +1,20 @@
+const titleFeatures = {
+    in: ["body"],
+    trim: true,
+    notEmpty:{
+        errorMessage: "The item should not be empty!",
+    },
+    isLength:{
+        min: 5,
+        max: 100,
+        errorMessage: "The item should have from 5 to 100 characters!"
+    }
+}
+
+export const titleAddValidationSchema = {
+    newItem: titleFeatures
+};
+
+export const titleUpdateValidationSchema ={
+    updatedItemTitle: titleFeatures
+}
